809c3f352076565068521d323d02080dce355e0f
joni2back <joni2back@gmail.com>
2015-02-19 12:24:42 -0300

    more updates

--- assets/js/selector-controller.js
+++ assets/js/selector-controller.js
@@ -11,10 +11,16 @@ FileManagerApp.controller('ModalFileManagerCtrl', [
     $scope.touch = function(item) {
         item = (item && item.revert && item) || new Item();
         $rootScope.selected = item;
+        $('#selector').modal('hide');
     };
 
     $scope.untouch = function() {
         $scope.touch(null);
+    };
+
+    $rootScope.openNavigator = function() {
+        $scope.untouch();
+        $('#selector').modal('show');
     };
 
     $scope.fileNavigator.refresh();
