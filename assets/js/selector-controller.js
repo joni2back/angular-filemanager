16a254cb95001d4a05b1f93038f31123a4b0339c
joni2back <joni2back@gmail.com>
2015-02-23 09:49:53 -0300

    fix dependency injection of filenavigator

--- assets/js/selector-controller.js
+++ assets/js/selector-controller.js
@@ -6,13 +6,12 @@
  */
 
 FileManagerApp.controller('ModalFileManagerCtrl', [
-    '$scope', '$rootScope', '$config', 'item', 'fileNavigator',
-    function($scope, $rootScope, $config, Item, FileNavigator) {
+    '$scope', '$rootScope', '$config', 'fileNavigator',
+    function($scope, $rootScope, $config, FileNavigator) {
 
     $scope.appName = $config.appName;
     $scope.orderProp = ['model.type', 'model.name'];
     $scope.fileNavigator = new FileNavigator();
-
 
     $rootScope.select = function(item, temp) {
         temp.tempModel.path = item.model.fullPath().split('/');
