6aaf19d603942ca8e49cf57eab20e2f4bfe5cf84
joni2back <joni2back@gmail.com>
2015-04-17 14:08:56 -0300

    remove unused imgs

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,6 +36,7 @@
     });
 
     app.filter('strLimit', ['$filter', function($filter) {
+        //going to use css3 ellipsis instead of this
         return function(input, limit) {
             if (input.length <= limit) {
                 return input;
