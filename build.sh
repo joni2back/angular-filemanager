adaa0f98bdca54d164722cc79be6aa52e72e6e5a
Jonas Sciangula Street <joni2back@gmail.com>
2015-04-21 09:51:49 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#download yui compressor here
 #https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
 
 YUI_COMPRESSOR_JAR="/opt/yuicompressor-2.4.8.jar"
@@ -7,7 +8,7 @@ MINIFIED_FILE="angular-filemanager.min.js"
 DIR_DIST="assets/js"
 MINIFIED_PATH="$DIR_DIST/$MINIFIED_FILE"
 
-find assets/js ! -name "$MINIFIED_FILE" -name "*.js" | sort | xargs -I {} cat {} > "$MINIFIED_PATH"
+find assets/js ! -name "$MINIFIED_FILE" -name "*.js" | sort | xargs -I "{}" cat "{}" > "$MINIFIED_PATH"
 #java -jar "$YUI_COMPRESSOR_JAR" "$MINIFIED_PATH" -o "$MINIFIED_PATH"
 
 echo "Complete at: $MINIFIED_PATH"
