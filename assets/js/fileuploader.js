f3c05b831c186e4785fc2f91bcad35fbfc029843
Patrick Hillert <patrick.hillert@inovex.de>
2015-06-24 12:04:52 +0200

    setting GET params when we only have one file to upload

--- assets/js/fileuploader.js
+++ assets/js/fileuploader.js
@@ -6,17 +6,29 @@
         self.requesting = false; 
         self.upload = function(fileList, path, success, error) {
             var form = new window.FormData();
- 
-            form.append('destination', '/' + path.join('/'));
-            for (var file in fileList) {
+
+            if (fileList.length == 1) {
+                // append get params
+                var prefix = '?';
+                if (fileManagerConfig.uploadUrl.indexOf('?') !== -1) {
+                    prefix = '&';
+                }
+                fileManagerConfig.uploadUrl += prefix + 'destination=' + '/' + path.join('/');
+
                 var fileObj = fileList[file];
-                typeof fileObj === 'object' && form.append('file-' + (1 + parseInt(file, null)), fileObj);
+                typeof fileObj === 'object' && form.append('file', fileObj);
+            } else {
+                for (var file in fileList) {
+                    var fileObj = fileList[file];
+                    typeof fileObj === 'object' && form.append('file-' + (1 + parseInt(file, null)), fileObj);
+                }
             }
 
             self.requesting = true;
+
             return $http.post(fileManagerConfig.uploadUrl, form, {
                 transformRequest: angular.identity,
-                headers: {'Content-Type': undefined}
+                headers: {'Content-Type': 'multipart/form-data'}
             }).success(function(data) {
                 self.requesting = false;
                 typeof success === 'function' && success(data);
