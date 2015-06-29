493c73cf26e6d9300ab54bb2ee872a39d8d2913c
Patrick Hillert <patrick.hillert@inovex.de>
2015-06-29 15:38:34 +0200

    adds navtree sort by name ascending

--- assets/js/filenavigator.js
+++ assets/js/filenavigator.js
@@ -63,6 +63,16 @@
                     }
                     parent.nodes.push({name: absName, nodes: []});
                 }
+
+                // sort nodes by name
+                parent.nodes = parent.nodes.sort(function(a, b) {
+                    if (a.name < b.name) {
+                        return -1;
+                    } else if (a.name > b.name) {
+                        return 1;
+                    }
+                    return 0;
+                });
             };
 
             !self.history.length && self.history.push({name: path, nodes: []});
