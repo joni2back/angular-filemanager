7fe90d6aa81aa4b6dbe7c30c19afaccce52f7d15
Jakub Ďuraš <jakub@duras.me>
2017-03-04 19:39:37 +0100

    Code styling improvements, readiblity improvements

--- bridges/php-local/LocalBridge/Translate.php
+++ bridges/php-local/LocalBridge/Translate.php
@@ -3,32 +3,32 @@ namespace AngularFilemanager\LocalBridge;
 
 /**
  * Translate class
- * 
+ *
  * For simple translation to alternative languages
- * @author 	  Jakub Ďuraš <jakub@duras.me> 
+ * @author Jakub Ďuraš <jakub@duras.me>
  */
 class Translate
 {
-	private $strings = [];
+    private $strings = [];
 
-	public function __construct($lang)
-	{
-		if(!file_exists('lang/'.$lang.'.json')){
-			throw new Exception('No language file for chosen language');
-			return;
-		}
+    public function __construct($lang)
+    {
+        if (!file_exists('lang/'.$lang.'.json')) {
+            throw new \Exception('No language file for chosen language');
+            return;
+        }
 
-		$json = file_get_contents('lang/'.$lang.'.json');
+        $json = file_get_contents('lang/'.$lang.'.json');
 
-		$this->strings = json_decode($json, true);
-	}
+        $this->strings = json_decode($json, true);
+    }
 
-	public function __get($name)
-	{
-		if(isset($this->strings[$name])){
-			return $this->strings[$name];
-		}else{
-			return $name;
-		}
-	}
+    public function __get($name)
+    {
+        if (isset($this->strings[$name])) {
+            return $this->strings[$name];
+        } else {
+            return $name;
+        }
+    }
 }
